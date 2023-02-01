c5f98320b44d44b2db6064ee66f4167720759a8b
erra-ch <73989943+erra-ch@users.noreply.github.com>
2023-02-01 11:16:19 +0100

    added integration test

---
+++ server/src/router/integration.test.ts
@@ -0,0 +1,18 @@
+import * as SuperTest from "supertest"
+import {Poll} from "../model/poll"
+import { app } from "../start"
+
+const request = SuperTest.default(app)
+
+test ("End-to-end test", async() =>{
+    const quest= "Questions anyone?";
+    const choi= ["A", "B", "C"];
+
+    const res1 = await request.post("/poll").send({"question" : quest, "choices" : choi});
+    expect(res1.statusCode).toEqual(201);
+    expect(res1.body.question).toEqual(quest);
+    const res2 = await request.get("/poll");
+    expect(res2.statusCode).toEqual(200);
+    expect(res2.body.question).toContain(quest)
+    }
+)
