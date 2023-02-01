53b5c0bc5f65597d02530f79c8f0be52b476dd7f
erra-ch <73989943+erra-ch@users.noreply.github.com>
2023-02-01 11:32:06 +0100

    Setup for lab3

--- server/src/start.ts
+++ server/src/start.ts
@@ -1,7 +1,9 @@
 import express from "express";
 import { pollRouter } from "./router/pollrouter";
+import cors from "cors";
 
 export const app = express();
 
 app.use(express.json());
+app.use(cors())
 app.use("/poll", pollRouter);
