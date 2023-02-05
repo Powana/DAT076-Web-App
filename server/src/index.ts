71bee0745ee33f1b61d05be54f6b2927a8b8a722
Ben <benw@student.chalmers.se>
2023-02-06 00:17:26 +0100

    Move app variables to config.ts, add placeholder directories

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,18 +1,39 @@
 import { app } from "./start";
+import { config } from "./config";
 
 
 /**
-* App Variables
+ * App Variables
 */
+const Sequelize = require("sequelize");
 
-
-const PORT : number = 8080;
-
+// Copy vars here as simple reminder that they need to be set
+const PORT : number = config.PORT;
+const DB_HOST = config.DB_HOST;
+const DB_NAME = config.DB_NAME;
+const DB_USER = config.DB_USER;
+const DB_PASS = config.DB_PASS;
 
 /**
 * Server Activation
 */
 
+const sequelize = new Sequelize(
+    DB_NAME,
+    DB_USER,
+    DB_PASS,
+    {
+        host: DB_HOST,
+        dialect: 'mysql',
+    
+        models: ["./models"]  // Relative path to our models
+    }
+);
+sequelize.authenticate().then(() => {
+        console.log('DB Connection has been established successfully.');
+    }).catch((error: any) => {
+        console.error('Unable to connect to the database: ', error);
+ });
 
 app.listen(PORT, () => {
     console.log(`listening on port ${PORT}`);
