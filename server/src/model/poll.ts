0a433ddbd5667e607b3e41641e6006b09f3828bb
ariadnadeprado <122884844+ariadnadeprado@users.noreply.github.com>
2023-02-08 21:49:31 +0100

    Add files via upload

--- server/src/model/poll.ts
+++ server/src/model/poll.ts
@@ -1,23 +1,30 @@
-    import { IChoice } from "./choice";
+import { IChoice } from "./choice";
 
     export class Poll {
+        id:number;
         question : string;
         choices :  Map<IChoice, number>;
+        comments:string[];
 
-        constructor(question: string, choices: Array<IChoice>) {
+        constructor(id:number,question: string, choices: Array<IChoice>) {
+            this.id=id;
             this.question = question;
             this.choices = new Map<IChoice, number>();
+            this.comments=[]; //you can not add comments when you create the poll
 
+
+            //el mapeado sirve para ver el numero de veces que ha sido votado
             choices.forEach(choice => {
                 this.choices.set(choice, 0);
             });
         }
 
         public toJSON() {
-            return {question: this.question, choices: Object.fromEntries(this.choices)}
+            return {id:this.id,question: this.question, choices: Object.fromEntries(this.choices),comments:this.comments}
         }
 
         public incrementCount(choice : IChoice): boolean {
+            
             let count = this.choices.get(choice);
             if (count == null) {
                 return false;
