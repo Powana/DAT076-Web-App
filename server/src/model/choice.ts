e3737e79be5fc676167e3f6ba9e8419d96907aa0
ariadnadeprado <122884844+ariadnadeprado@users.noreply.github.com>
2023-02-11 14:36:45 +0100

    Add files via upload

--- server/src/model/choice.ts
+++ server/src/model/choice.ts
@@ -1,31 +1,41 @@
 export interface IChoice {
+    incrementCount(): boolean;
     toString(): string;
 }
-
-
 export class TextChoice implements IChoice {
     text : string;
-
+    count : number;
+    
     constructor(text: string) {
         this.text = text;
+        this.count=0;
     }
 
     public toString = () : string => {
         return this.text;
-    }
-    
-}
+    }   
 
+    public incrementCount=():boolean=>{
+        this.count=this.count+1;
+        return true;
+    }
+
+}
 // Example of another type of choice we could have
 export class DateChoice implements IChoice {
     date : Date;
+    count:number;
 
     constructor(date : Date) {
         this.date = date;
+        this.count=0;
     }
 
     public toString = () : string => {
         return this.date.toLocaleDateString();
     }
-    
+    public incrementCount=():boolean=>{
+        this.count=this.count+1;
+        return true;
+    }
 }
