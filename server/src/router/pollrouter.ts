5b218b230a0f94a7c51f7e76d6e55b0bfbcd8452
ariadnadeprado <122884844+ariadnadeprado@users.noreply.github.com>
2023-02-11 14:37:06 +0100

    Add files via upload

--- server/src/router/pollrouter.ts
+++ server/src/router/pollrouter.ts
@@ -5,6 +5,9 @@ import { IChoice, TextChoice } from "../model/choice";
 
 export const pollRouter = express.Router();
 const pollService = new PollService();
+let contador=0;
+
+//http://localhost:PORT/poll/
 
 pollRouter
     .get('/',(req : Request, res : Response) => {
@@ -21,10 +24,9 @@ pollRouter
         }
     })
 
-    //crear poll
-    .post('/',(req : Request<{}, {}, {id:number,question: string, choices: Array<any>}>, res) => {
+    //crear first poll
+    .post('/firstcreate',(req : Request<{}, {}, {question: string, choices: Array<any>}>, res) => {
         try {
-            const id: number = req.body["id"]
             const question: string = req.body["question"]
             const raw_choices: Array<string> = req.body["choices"] 
             
@@ -36,10 +38,10 @@ pollRouter
                 res.status(400).send("Polls must have 3 choices")
             }
 
-            let newPoll = pollService.createPollFromAny(id,question, raw_choices);
-            
+            let newPoll = pollService.createPollFromAny(question, raw_choices);
+
             PollService.thePoll = newPoll; // For demonstration purposes TODO: Remove
-            
+            contador=contador+1;
             res.status(201).send(newPoll.toJSON());
 
         } catch (e: any) {
@@ -47,34 +49,136 @@ pollRouter
         }
     })
 
-    //responder poll
-    .put('/answer',(req: Request<{}, {}, {id:number,choice: IChoice}>, res) => {
+    
+    //add answer
+    .post('/addanswer',(req : Request<{}, {}, {id:number,choice: string}>, res) => {
         try {
             const id: number = req.body["id"]
-            const choice: IChoice = req.body["choice"]
+            const answer: string = req.body["choice"]
             
-            if (choice == null) {
+            if (answer == null || id>=contador) {
                 res.status(400).send("Invalid payload")
             }
             
-            res.status(200).send(pollService.incrementCount(id,choice));
+            else{
+                let newPoll = pollService.add_answer(id,answer);
+            
+                PollService.thePoll = newPoll; 
+                
+                res.status(201).send(newPoll.toJSON());
+            }
+        
 
         } catch (e: any) {
             res.status(500).send(e.message);
         }
     })
 
-    //añadir comentario
+    
+    //answer poll with single answer
+    .put('/answer',(req: Request<{}, {}, {id:number,choice: string}>, res) => {
+        try {
+            const id: number = req.body["id"]
+            const choice: string = req.body["choice"]
+            
+            if (choice == null || id>=contador) {
+                res.status(400).send("Invalid payload")
+            }
+            
+            res.status(200).send(pollService.chosepoll(id).incrementCount(choice));
+        } catch (e: any) {
+            res.status(500).send(e.message);
+        }
+    })
+
+    
+    //add comment
     .put('/comment',(req:Request<{},{},{id:number,comment:string}>,res)=>{
         try {
             const id: number = req.body["id"]
             const comment: string = req.body["comment"]
             
-            if (comment == null) {
+            if (comment == null || id>=contador) {
                 res.status(400).send("Invalid payload")
             }
             
             res.status(200).send(pollService.addcomment(id,comment));
+
+        } catch (e: any) {
+            res.status(500).send(e.message);
+        }
+    })
+
+    //obtain exact poll
+    .put('/concretepoll',(req : Request<{}, {}, {id: number}>, res : Response) => {
+        try {
+            const id: number = req.body["id"]
+
+            if (id>= contador) {  // At the moment, only allow for one poll for simple testing purposes.
+                res.status(200).send("No poll has been created");
+
+            }
+            else {
+                res.status(200).send(pollService.chosepoll(id))
+            }
+        } catch (e: any) {
+            res.status(500).send(e.message);
+        }
+    })
+
+    //get all polls
+    .get('/allpolls',(req : Request, res : Response) => {
+        try {
+            const id: number = req.body["id"]
+
+            if (pollService.polls == null) {  // At the moment, only allow for one poll for simple testing purposes.
+                res.status(200).send("No poll has been created");
+
+            }
+            else {
+                res.status(200).send(pollService.polls)
+            }
+        } catch (e: any) {
+            res.status(500).send(e.message);
+        }
+    })
+
+    //crear others poll
+    .post('/create',(req : Request<{}, {}, {question: string, choices: Array<any>}>, res) => {
+        try {
+            const question: string = req.body["question"]
+            const raw_choices: Array<string> = req.body["choices"] 
+            
+            if (question == null || raw_choices == null) {
+                res.status(400).send("Invalid payload")
+            }
+            
+            if (raw_choices.length != 3) {  // For demonstration purposes TODO: Remove
+                res.status(400).send("Polls must have 3 choices")
+            }
+
+            let newPoll = pollService.createPoll(contador,question, raw_choices);
+            contador=contador+1;
+            res.status(201).send(newPoll.toJSON());
+
+        } catch (e: any) {
+            res.status(500).send(e.message);
+        }
+    })
+
+
+   
+    //answer poll with multiple answer
+    .put('/multipleanswer',(req: Request<{}, {}, {id:number,choices: Array<string>}>, res) => {
+        try {
+            const id: number = req.body["id"]
+            const choices:  Array<string> = req.body["choices"]
+            
+            if (choices == null || id>=contador) {
+                res.status(400).send("Invalid payload")
+            }
+            
+            res.status(200).send(pollService.chosepoll(id).incrementCounts(choices));
 
         } catch (e: any) {
             res.status(500).send(e.message);
