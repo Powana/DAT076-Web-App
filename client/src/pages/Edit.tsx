d0e78746ef96b6f977454874cbf8f704c97caebd
Ben <benw@student.chalmers.se>
2023-02-20 02:44:01 +0100

    Move PollEditApp to Edit

--- client/src/pages/Edit.tsx
+++ client/src/pages/Edit.tsx
@@ -1,10 +1,49 @@
-function Edit() {
+import React, {useState} from 'react';
+import { Button, Form, FormControl, FormGroup, FormLabel } from 'react-bootstrap';
+import './Edit.css';
 
-    return (
-      <div className="Edit">
-        <h2>Edit</h2>
-      </div>
-    )
-  }
-  
-  export default Edit
+function Edit() {
+  const [question, setQuestion] = useState<string>();
+  const [choices, setChoices] = useState<any[]>();
+
+  React.useEffect(() => {
+
+    fetch("/poll")
+      .then((res) => res.json())
+      .then((data) => {
+          setQuestion(data.question);
+          setChoices(data.choices);
+      })
+      .catch((err) => {
+        console.log(err.message);
+     });
+  }, []);
+
+  return (
+    <div className="Edit">
+      <Form className="mb-3">
+        <FormGroup>
+          <FormLabel>Question</FormLabel>
+          <FormControl type="text" value={question}/>
+        </FormGroup>
+        
+        <FormGroup>
+          <FormLabel>Choices:</FormLabel>
+          {choices?.map(choice => {
+              return (
+              <FormGroup>
+                <FormLabel>Choice</FormLabel>
+                <FormControl type="text" value={choice}/>
+              </FormGroup>
+              )
+            })}
+        </FormGroup>
+        <Button variant="primary" type="submit">
+          Save
+        </Button>
+      </Form>
+    </div>
+  );
+}
+
+export default Edit;
