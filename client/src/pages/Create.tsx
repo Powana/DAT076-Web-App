8c65c07c5f332df5876759c605879d5d40e08008
chrmeis <chrmeis@student.chalmers.se>
2023-02-21 11:46:27 +0100

    not reading fron choicefield yet

--- client/src/pages/Create.tsx
+++ client/src/pages/Create.tsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react';
 import './Create.css';
 import Button from 'react-bootstrap/Button';
 import axios from 'axios';
-
 import Toast from 'react-bootstrap/Toast';
 
 function BasicExample() {
@@ -19,55 +18,7 @@ function BasicExample() {
 }
 
 
-interface IChoice {
-  id: number;
-  choicetext: string;
-}
-
-interface Poll {
-  id: number;
-  question: string;
-  choices: Map<IChoice, number>;
-}
-
-function Questionfield() {
-  return (
-    <div className="Questionbox">
-      <h2> Create a question </h2>
-      <form>
-        <label>Enter question</label>
-        <input type="text" placeholder="Question" name="ques"></input>
-      </form>
-    </div>
-  );
-}
-
-interface ChoiceItemProps {
-  key: number;
-  choicetext: string;
-}
-
-
-function Choicefield() {
-  return (
-    <div className="Choicebox">
-      <form>
-        <label>Enter answer</label>
-        <input type="text" placeholder="option" name="ans"></input>
-      </form>
-    </div>
-  );
-};
-
-
 /*
-function ChoiceItem({choicetext}:ChoiceItemProps) {
-  return (
-    Choicefield
-  );
-};
-*/
-
 function ChoiceItem({ choicetext }: ChoiceItemProps) {
   return (
     <div className="Choicebox">
@@ -78,33 +29,114 @@ function ChoiceItem({ choicetext }: ChoiceItemProps) {
     </div>
   );
 };
+*/
 
-/*
-function Add_answer() {
+
+
+
+function Choicefield({handleCChange} :{handleCChange:(event:React.ChangeEvent<HTMLInputElement>) => void} ){
   return (
-    <div className="Button">
-      <Button variant="primary" onClick= {moreChoice}>add Answer</Button>{''}
+    <div className="Choicebox">
+      <form>
+        <label>Enter answer</label>
+        <input type="text" placeholder="option" name="choiceArray" onChange={handleCChange}/>
+      </form>
     </div>
   );
 };
-*/
+
+function Questionfield({handleQChange} :{handleQChange:(event:React.ChangeEvent<HTMLInputElement>) => void} ){
+  return (
+    <div className="Questionbox">
+      <h2> Create a question </h2>
+      <form>
+        <label>Enter question</label>
+        <input type="text" placeholder="Question" name="question" onChange={handleQChange}></input>
+      </form>
+    </div>
+  );
+}
 
 
 function Submit() {
   return (
     <div className="Button">
-      <Button variant="primary" onClick={() => BasicExample()}>Submit</Button>{' '}
+      <Button variant="primary" >Submit</Button>{' '}
     </div>
   );
 }
 
+<<<<<<< Updated upstream:client/src/pages/Create.tsx
 function Create() {
+=======
+/*
+function Submit({handleSubmit} :{handleSubmit:(event:React.MouseEvent<HTMLButtonElement>) => void} ) {
+  return (
+    <div className="Button">
+      <Button variant="primary" onClick={handleSubmit}>Submit</Button>{' '}
+    </div>
+  );
+}
+*/
+
+function App() {
+  interface IChoice {
+    id: number;
+    choicetext: string;
+  }
+  
+  interface Poll {
+    id: number;
+    question: string;
+    choices: Map<IChoice, number>;
+  }
+  
+  interface ChoiceItemProps {
+    key: number;
+    choicetext: string;
+  }
+  
+  
+>>>>>>> Stashed changes:client/src/App.tsx
   const [iChoices, setIChoicees] = useState<IChoice[]>([])
   const [numChoices, setNumChoices] = useState<number>(3);
-  
-  function handlePlus() {
-    setNumChoices(numChoices => numChoices + 1);
+  const [question, setQuestion] = useState<string>()
+  const [choices, setChoices] = useState<string[]>()
+
+  //TODO fix this function, look att lecturecode
+  useEffect(()=>
+  async function uppdatePoll(){
+    const response = await axios.push<Poll>("http://localhost:8080/poll;")
+  }
+  )
+
+  function handleQChange(event: React.ChangeEvent<HTMLInputElement>){
+    setQuestion(event.target.value)
+    console.log(question);
   };
+
+  function handleCChange(event: React.ChangeEvent<HTMLInputElement>){
+    console.log(choices);
+  };
+/* 
+  function handleCChange1(event: React.ChangeEvent<HTMLInputElement>){
+    setChoices(choices.push(event.target.value))
+    console.log(choices);
+  };
+
+  function handleCChange2(event: React.ChangeEvent<HTMLInputElement>){
+    setChoices((prev)=>{
+       return{...prev, choices}
+    });
+    console.log(choices);
+  };
+
+  function handleCChange3(event: React.ChangeEvent<HTMLInputElement>){
+    choices.push(setChoices(event.target.value))
+    console.log(choices);
+  };
+  */
+  
 
   function Plus_answer() {
     return (
@@ -113,6 +145,17 @@ function Create() {
       </span>
     );
   };
+
+  function handlePlus() {
+    setNumChoices(numChoices => numChoices + 1);
+  };
+
+  function handleSubmit (event: React.MouseEvent<HTMLButtonElement>){
+    event.preventDefault();
+    console.log()
+  }
+
+  
 
 
   return (
@@ -127,13 +170,13 @@ function Create() {
           <div className="col"></div>
           <div className="col">
             <div className="NewPoll">
-              <Questionfield />
+              <Questionfield handleQChange = {handleQChange} />
               <div className="choiceBox">
                 <h2 >Possible answers </h2>
                 <div>
                   {Array.from(Array(numChoices).keys())
                     .map((n: number) => {
-                      return <Choicefield key={n} />
+                      return <Choicefield key={n} handleCChange = {handleCChange}  />
                     })}
                 </div>
               </div>
